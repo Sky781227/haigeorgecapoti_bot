f266cfd01fa3dcef833ca5644ab7c20e881f163f
Robert Kovacs <robi.kovacs11@gmail.com>
2016-01-28 12:51:16 +0200

    updated messages

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -35,9 +35,6 @@ class External
   end
 end
 
-motivatie = ["Some people die at 25 and aren’t buried until 75. – Benjamin Franklin", "Here is the test to find whether your mission on Earth is finished: if you’re alive, it isn’t. – Richard Bach", "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma — which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition. They somehow already know what you truly want to become. Everything else is secondary. – Steve Jobs"]
-cesafac = ["Quit bitching about how tired you are.", "If the motivation you need is to work on building your own business", "Put a pot of coffee on or get some tea brewing.", "Turn off email, Twitter, Facebook, Pinterest and whatever other social procrastination network you’re part of.", "Now, put on your favorite music playlist."]
-
 bot = TelegramBot.new(token: '168908536:AAEr3XjROJDtwIlWIPkfCjgwIeNxdYYJVjI')
 logger.debug "starting telegram bot"
 
@@ -48,9 +45,9 @@ bot.get_updates(fail_silently: true) do |message|
   message.reply do |reply|
     case command
     when /motivatie/
-      reply.text = motivatie.sample
+      reply.text = External.motivate_the_shit_out_of_george
     when /cesafac/
-      reply.text = cesafac.sample
+      reply.text = External.motivate_the_shit_out_of_george
     when /bagauncitat/
       reply.text = External.motivate_the_shit_out_of_george
     else
