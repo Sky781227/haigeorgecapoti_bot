5b9bddfec5ea452729674eb11f0737b02adfc7ac
Florin Ionce <ionce.florin@gmail.com>
2016-01-30 20:57:59 +0200

    cleanup code - remove widget component

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130161537) do
+ActiveRecord::Schema.define(version: 20160130185333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,13 +42,5 @@ ActiveRecord::Schema.define(version: 20160130161537) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "widgets", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
